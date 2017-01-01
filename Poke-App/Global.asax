17aecfb22c05a03f5fbbe2afbaf9198b4618b270
Heriberto Basulto <hdotbasulto@gmail.com>
2017-01-01 12:47:24 -0800

    Add project files.

---
+++ Poke-App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Poke_App.MvcApplication" Language="C#" %>
---
+++ Poke-App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Poke_App.Startup))]
+namespace Poke_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
